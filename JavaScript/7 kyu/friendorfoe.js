1225dac804fc06af44616a1156fa036c8c39a07d
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-05-01 14:02:53 -0600

    created instructions and solution to javascript problem

---
+++ JavaScript/7 kyu/friendorfoe.js
@@ -0,0 +1,20 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// Solution:
+
+function friend(friends){
+  var word = [];
+  var final = [];
+  for(var i = 0; i < friends.length; i++) {
+    word = friends[i].split('')
+      console.log(word.length)
+      if (word.length == 4) {
+        final.push(word.join(''))
+      } 
+  }
+  return final
+}
